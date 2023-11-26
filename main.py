39f540b3b3c9e57a85cdc06b5430abcbf33984bb
shumeraa <116764970+shumeraa@users.noreply.github.com>
2023-11-26 17:54:43 -0500

    Added classMap
    Co-authored-by: micklerj <micklerj@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 import requests
+import json
 
-response = requests.get('https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2235')
-print(response.text)
+
+classMap = {}
+#stores course code, course ID, course name, prerequisites
+
+#Spring 2024 class schedule, first 50 classes
+response = requests.get('https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2241&last-control-number=0') 
+data = json.loads(response.text)
+
+courses = data[0]['COURSES']
+for course in courses:
+    code = course.get('code', 'Unknown Code')
+    courseId = course.get('courseId', 'Unknown ID')
+    name = course.get('name', 'Unknown Name')
+    prerequisites = course.get('prerequisites', 'No Prerequisites')
+    classMap[code] = [courseId, name, prerequisites]
+    #print(courseCode, courseID, courseName, prereqs)
+
+x = classMap['ABE3212C']
+print(x)
+
+
+# for i in range(50):
+#     courseCode = data[i]['code']
+#     courseID = data[i]['courseId']
+#     courseName = data[i]['name']
+#     prereqs = data[i]['prerequisites']
+#     #classMap[courseCode] = [courseID, courseName, prereqs]
+#     print(courseCode, courseID, courseName, prereqs)
+
+
+
