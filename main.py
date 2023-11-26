a77955ead8ab9c3142795b1348faec363be3a782
shumeraa <116764970+shumeraa@users.noreply.github.com>
2023-11-26 18:53:58 -0500

    Fixed adding classes to classMap

--- main.py
+++ main.py
@@ -4,31 +4,27 @@ import json
 
 classMap = {}
 #stores course code, course ID, course name, prerequisites
+last_row = 0
+session = requests.Session()
 
 #Spring 2024 class schedule, first 50 classes
-response = requests.get('https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2241&last-control-number=0') 
-data = json.loads(response.text)
+while True:
+    response = session.get(f'https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2241&last-control-number={last_row}') 
+    data = json.loads(response.text)
 
-courses = data[0]['COURSES']
-for course in courses:
-    code = course.get('code', 'Unknown Code')
-    courseId = course.get('courseId', 'Unknown ID')
-    name = course.get('name', 'Unknown Name')
-    prerequisites = course.get('prerequisites', 'No Prerequisites')
-    classMap[code] = [courseId, name, prerequisites]
-    #print(courseCode, courseID, courseName, prereqs)
+    courses = data[0]['COURSES']
+    for course in courses:
+        code = course.get('code', 'Unknown Code')
+        courseId = course.get('courseId', 'Unknown ID')
+        name = course.get('name', 'Unknown Name')
+        prerequisites = course.get('prerequisites', 'No Prerequisites')
+        classMap[code] = [courseId, name, prerequisites]
+        if code == '0000':
+            break         
 
-x = classMap['ABE3212C']
-print(x)
+    last_row += len(courses)
+    print(last_row)
 
-
-# for i in range(50):
-#     courseCode = data[i]['code']
-#     courseID = data[i]['courseId']
-#     courseName = data[i]['name']
-#     prereqs = data[i]['prerequisites']
-#     #classMap[courseCode] = [courseID, courseName, prereqs]
-#     print(courseCode, courseID, courseName, prereqs)
 
 
 
