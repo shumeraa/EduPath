f47cd60b20fda33984c4310af32311a386eb760c
shumeraa <116764970+shumeraa@users.noreply.github.com>
2023-11-28 15:54:16 -0500

    Dowloading data as JSON

--- fetch_and_pickle.py
+++ fetch_and_pickle.py
@@ -2,41 +2,74 @@ import requests
 import json
 import pickle
 
-
-
-classMap = {}
-#stores course code, course ID, course name, prerequisites
-last_row = 0
+all_data = []
+all_data.append("[{\"COURSES\":")
 session = requests.Session()
-departments = set()
 break_flag = False
+last_row = 0
 
 
-#Spring 2024 class schedule, first 50 classes
+
 while True:
     response = session.get(f'https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2241&last-control-number={last_row}') 
     data = json.loads(response.text)
     courses = data[0]['COURSES']
     for course in courses:
-        if code == '0000':
+        code = course.get('code', '0000')
+        if code == '0000' or last_row > 300:
             break_flag = True
             break
-        code = course.get('code', 'Unknown Code')
-        courseId = course.get('courseId', 'Unknown ID')
-        name = course.get('name', 'Unknown Name')
-        prerequisites = course.get('prerequisites', 'No Prerequisites')
-        #deptName = course.get('sections')[0].get('deptName')
-        classMap[code] = [courseId, name, prerequisites]
-        print(code)
+        print(course)
+        all_data.append(course)
+    if break_flag:
+         break
+    last_row += len(courses)
+    print(len(all_data))
+
+
+all_data.append("}]")
+with open('all_data.json', 'w') as f:
+    json.dump(all_data, f)
+
+
+# classMap = {}
+# stores course code, course ID, course name, prerequisites
+# last_row = 0
+# session = requests.Session()
+# departments = set()
+# break_flag = False
+
+#Spring 2024 class schedule, first 50 classes
+# while True:
+#     response = session.get(f'https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2241&last-control-number={last_row}') 
+#     data = json.loads(response.text)
+#     courses = data[0]['COURSES']
+#     for course in courses:
+#         code = course.get('code', 'Unknown Code')
+#         if code == '0000':
+#             break_flag = True
+#             break
+#         courseId = course.get('courseId', 'Unknown ID')
+#         name = course.get('name', 'Unknown Name')
+#         prerequisites = course.get('prerequisites')
+#         sectionsVector = 
+#         sections = course.get('sections')
+#         for section in sections:
+#             classNumber = section.get('classNumber')
+#             key = f"{code}_{courseId}_{classNumber}"
+#             classMap[key] = [name, prerequisites]
+#             print(key)
+#         #deptName = course.get('sections')[0].get('deptName')
+        
                  
         
-    if break_flag:
-        break
-    last_row += len(courses)
-    print(len(classMap))
+#     if break_flag:
+#         break
+#     last_row += len(courses)
+#     print(len(classMap))
 
-with open('classMap.pkl', 'wb') as f:
-    pickle.dump(classMap, f)
+# with open('classMap.json', 'w') as f:
+#     json.dump(classMap, f)
 
 
 
--- main.py
+++ main.py
@@ -8,3 +8,17 @@ with open('classMap.pkl', 'rb') as f:
 
 print(classMap['AEB3510'])
 
+with open('all_data.json', 'r') as f:
+    all_Courses = json.load(f)
+
+classMap = {}
+
+while True:
+    courses = all_Courses['COURSES']
+    for course in courses:
+        name = course.get('name')
+        print(name)
+        sections = course.get('sections')
+        for section in sections:
+            classNumber = section.get('classNumber')
+            print(classNumber)
