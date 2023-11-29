3b2cb26959adcdd4ebdfca4978213c0f425b17d1
shumeraa <116764970+shumeraa@users.noreply.github.com>
2023-11-28 20:49:50 -0500

    Branch Test

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ with open('all_data.json', 'r') as f:
 tree_obj = tree.classTree()
 
 for course in all_Courses:
+
     code = course.get('code')
     name = course.get('name')
     prerequisites = course.get('prerequisites')
     tree_obj.insert(code, prerequisites)
 
+
+
+
+
+#IDS2935 Quest2 course
+#The world and big data
+#example2
 for course in all_Courses:
     code = course.get('code')
     vector = tree_obj.get_vector(code)
     print(vector)
-    
+
 tree_obj.insert("EEC6905", ['Prereq: ADV 3008 and MAR 3023 with minimum grades of C and Advertising major of junior standing or higher.'])  # Insert a class code with its corresponding vector
 vector = tree_obj.get_vector("EEC6905")  # Retrieve the vector for a given class code
 print(vector)
@@ -24,7 +32,7 @@ print(vector)
 
 
 
-count = 0
+# count = 0
 
 # for course in all_Courses:
 #     print(count)
