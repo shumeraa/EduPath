f81fad385b9458504db951e324ba45a6babfdaff
mgontu <100733195+mgontu@users.noreply.github.com>
2023-12-03 15:34:15 -0500

    update 3:30pm

--- dataGenerator.py
+++ dataGenerator.py
@@ -81,13 +81,30 @@ def assign_prerequisites(courses):
 
     return courses
 
-courses = create_additional_courses()
+def update_course_data(limit=100000):
+    with open('all_data.json', 'r') as f:
+        all_courses = json.load(f)
+    existing_course_count = len(all_courses)
+    print(f"Existing courses: {existing_course_count}")
 
-# assign prerequisites to a third of the courses
-courses_with_prereqs = assign_prerequisites(courses)
+    courses_to_add = max(limit - existing_course_count, 0)
+    print(f"New courses to be added: {courses_to_add}")
 
-# save the courses with prerequisites to a new JSON file
-with open('updated_courses_data.json', 'w') as file:
-    json.dump(courses_with_prereqs, file, indent=4)
+    if courses_to_add > 0:
+        # Generate new courses
+        new_courses = create_additional_courses(courses_to_add)
+        
+        # Assign prerequisites only to the new courses
+        new_courses_with_prereqs = assign_prerequisites(new_courses)
 
-    
+        # Combine with existing courses
+        all_courses += new_courses_with_prereqs
+    else:
+        print("Already have enough courses")
+
+    with open('updated_courses_data.json', 'w') as file:
+        json.dump(all_courses, file, indent=4)
+
+    print(f"Total courses after update: {len(all_courses)}")
+
+update_course_data()
